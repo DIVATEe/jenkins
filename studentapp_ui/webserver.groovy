415743235b25016c52835e7d847056eef090f2f8
Amit_Divate <adivate49@gmail.com>
2026-01-06 13:20:29 +0530

    pipeline updated

--- studentapp_ui/webserver.groovy
+++ studentapp_ui/webserver.groovy
@@ -17,17 +17,20 @@ pipeline {
         }
         stage('test') {
             steps {
-                sh '''mvn clean verify sonar:sonar \\
-                -Dsonar.projectKey=student-apps \\
-                -Dsonar.host.url=http://3.110.134.171:9000 \\
-                -Dsonar.login=sqp_925643351c72c64c1ee6b703d7f0d850eb7d0e8f'''
+                sh '''mvn clean verify sonar:sonar \
+                -Dsonar.projectKey=studentapp \
+                -Dsonar.host.url=http://52.66.215.114:9000 \
+                -Dsonar.login=sqp_ad616655247c1bee0b6ac4822e974dba1282704c'''
             }
         }
 
          stage('deploy') {
             steps {
-            sh '''sudo mv /home/ubuntu/workspace/studentapp/target/studentapp-2.2-SNAPSHOT.war student.war
-            sudo cp -rf /home/ubuntu/workspace/studentapp/student.war /opt/apache-tomcat-9.0.113/webapps/
+            sh '''sudo mv /home/ubuntu/workspace/webserver/target/studentapp-2.2-SNAPSHOT.war student.war
+            sudo cp -rf /home/ubuntu/workspace/webserver/student.war /opt/apache-tomcat-9.0.113/webapps/
+            sudo aws s3 cp /opt/apache-tomcat-9.0.113/webapps/student.war s3://cbz-app/
+            sudo cp /home/ubuntu/workspace/webserver/mysql-connector.jar /opt/apache-tomcat-9.0.113/lib/
+            sudo cp /home/ubuntu/workspace/webserver/context.xml /opt/apache-tomcat-9.0.113/conf/
             sudo sh /opt/apache-tomcat-9.0.113/bin/catalina.sh start
             sudo echo "we are in deploy stage"'''
             }
